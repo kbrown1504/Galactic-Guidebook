f62ee5a0d355e504385eaefb38c3cf7d0f5aef25
Keegan Brown <keegqbrown@gmail.com>
2020-06-18 16:50:16 -0500

    Initial Commit

---
+++ Galactic Guidebook/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Galactic Guidebook
+//
+//  Created by Keegan Brown on 6/18/20.
+//  Copyright © 2020 Keegan Brown. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Galactic Guidebook/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Galactic Guidebook
+//
+//  Created by Keegan Brown on 6/18/20.
+//  Copyright © 2020 Keegan Brown. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
