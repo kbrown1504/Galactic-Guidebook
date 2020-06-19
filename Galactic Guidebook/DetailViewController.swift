b74b8e5378c448ebcb151c8193adc6450c2f66a2
Carly Cameron <cmcameron@unomaha.edu>
2020-06-18 20:51:57 -0500

    basics of scroll view

---
+++ Galactic Guidebook/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Galactic Guidebook
+//
+//  Created by Carly Cameron on 6/18/20.
+//  Copyright © 2020 Keegan Brown. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var nameOfPlanetLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        nameOfPlanetLabel.text = " AAHHAHAHAHHAAHHAHAHAHAHAH AHHAHAHHAHAHA HAHAHAHHAHAHHA AHHAH AHHA HAhHAHAHHHAHAhahahha HAHa ahhaha "
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
