2ebdce7cd506e6245bd500a41562edfbe9d695eb
Carly Cameron <cmcameron@unomaha.edu>
2020-06-20 01:24:00 -0500

    make it look good

--- Galactic Guidebook/DetailViewController.swift
+++ Galactic Guidebook/DetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var circlePlanetImage: UIImageView!
     @IBOutlet weak var nameOfPlanetLabel: UILabel!
     @IBOutlet weak var longDescriptionLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
@@ -17,6 +18,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var yearLengthLabel: UILabel!
     @IBOutlet weak var funFactLabel: UILabel!
     @IBOutlet weak var planetImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,14 +26,20 @@ class DetailViewController: UIViewController {
         funFactLabel.text = "A really fun fact"
         yearLengthLabel.text = "1000 days"
         diameterLabel.text = "212345 "
-        nameOfPlanetLabel.text = " Pluto "
+        nameOfPlanetLabel.text = " Mars"
         distanceLabel.text = "1234567 miles"
         longDescriptionLabel.text = "https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly. https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly. https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly.https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly. https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly. https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly. https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly. https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly. https://github.com/ccameron1/PizzaHunter.git some really long description that will make the scroon get bigger. This will be extra long to prove that it scrolls really goodly.  "
+        
+        
+        
+       
     }
     
 
     @IBAction func onLinkPressed(_ sender: Any) {
     }
+    
+    
     /*
     // MARK: - Navigation
 
