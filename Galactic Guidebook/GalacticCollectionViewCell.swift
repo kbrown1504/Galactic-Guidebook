70284392e14c3bb8cf747797204aa43ebc84505b
Keegan Brown <keegqbrown@gmail.com>
2020-06-22 08:04:09 -0500

    just comments

--- Galactic Guidebook/GalacticCollectionViewCell.swift
+++ Galactic Guidebook/GalacticCollectionViewCell.swift
@@ -8,6 +8,16 @@
 
 import UIKit
 
+/*
+ Creating the custom cell is a bit difficult, but I'll try to describe the process here.
+ 1. Create this file and then the outlets.
+ 2. Set up your scroll view to have a reusable cell.
+ 3. Drag an imageView and Label into the content view of the reusable cell.
+ 4. Connect the outlets.
+ 5. Confirm that your tableView cell in the storyboard has its custom class set to GalactiCollectionViewCell.
+ */
+
+
 //Custom Collection View Controller
 class GalacticCollectionViewCell: UICollectionViewCell {
     //Outlet for imageView and label
